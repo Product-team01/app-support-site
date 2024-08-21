150b7cd178ca046179c5091c1925827c2ed6f8ad
silasashar1 <silas@madstreetden.com>
2024-08-21 16:06:29 +0530

    lms5

--- src/pages/LMSHOME/Index.jsx
+++ src/pages/LMSHOME/Index.jsx
@@ -1,3 +1,90 @@
+// import React, { useState } from 'react';
+// import Layout from '@theme/Layout';
+// import Title from './Title';
+// import FilterBar from './FilterBar';
+// import CourseCard from './CourseCard';
+
+// const CoursesData = [
+//   {
+//     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/type2.png',
+//     title: 'Document Types',
+//     organization: 'MSD',
+//     level: 'Beginner',
+//     type: 'Course',
+//     href: '/LMS',
+//     category: 'Document'
+//   },
+//   {
+//     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/newuser1.png',
+//     title: 'Datasets',
+//     organization: 'Document',
+//     level: 'Beginner',
+//     type: 'Specialization',
+//     href: '/LMS-2',
+//     category: 'Document'
+//   },
+//   {
+//     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/newuser1.png',
+//     title: 'Dataset Groups',
+//     organization: 'Document',
+//     level: 'Intermediate',
+//     type: 'Course',
+//     href: '/LMS-3',
+//     category: 'Document'
+//   },
+//   {
+//     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/connect5b.png',
+//     title: 'Connections',
+//     organization: 'Document',
+//     level: 'Advanced',
+//     type: 'Course',
+//     href: '/LMS-4',
+//     category: 'Document'
+//   },
+//   {
+//     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/connect5b.png',
+//     title: 'Model Gen',
+//     organization: 'Document',
+//     level: 'Advanced',
+//     type: 'Course',
+//     href: '/LMS-7',
+//     category: 'Model Gen'
+//   }
+// ];
+
+// const Main = () => {
+//   const [selectedCategory, setSelectedCategory] = useState('All');
+  
+//   const categories = ['All', 'Document','Model Gen'];
+  
+//   const handleCategoryChange = (category) => {
+//     setSelectedCategory(category);
+//   };
+
+//   const filteredCourses = selectedCategory === 'All'
+//     ? CoursesData
+//     : CoursesData.filter(course => course.category === selectedCategory);
+
+//   return (
+//     <Layout>
+//       <header className="unique-app-header" />
+//       <Title />  {/* Title Component */}
+//       <FilterBar 
+//         categories={categories} 
+//         selectedCategory={selectedCategory} 
+//         onCategoryChange={handleCategoryChange}  
+//       />
+//       <div className="course-grid">
+//         {filteredCourses.map((course, index) => (
+//           <CourseCard key={index} {...course} />
+//         ))}
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// export default Main;
+
 import React, { useState } from 'react';
 import Layout from '@theme/Layout';
 import Title from './Title';
@@ -10,45 +97,60 @@ const CoursesData = [
     title: 'Document Types',
     organization: 'MSD',
     level: 'Beginner',
-    type: 'Course',
+    type: 'Module',
     href: '/LMS',
-    category: 'Document'
+    category: 'Document',
+    description: 'Introduction to Document Types, what they are, and how to create new document types'
   },
   {
     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/newuser1.png',
     title: 'Datasets',
     organization: 'Document',
     level: 'Beginner',
-    type: 'Specialization',
+    type: 'Module',
     href: '/LMS-2',
-    category: 'Document'
+    category: 'Document',
+    description: 'Introduction to Datasets, what they are, operations that are possible and how to use them'
   },
   {
     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/newuser1.png',
     title: 'Dataset Groups',
     organization: 'Document',
     level: 'Intermediate',
-    type: 'Course',
+    type: 'Module',
     href: '/LMS-3',
-    category: 'Document'
+    category: 'Document',
+    description: 'Introduction to Dataset Groups, what they can be used for and how to use them'
   },
   {
     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/connect5b.png',
     title: 'Connections',
     organization: 'Document',
-    level: 'Advanced',
-    type: 'Course',
+    level: 'Intermediate',
+    type: 'Module',
     href: '/LMS-4',
-    category: 'Document'
+    category: 'Document',
+    description: 'Introduction to Connections, what they are, and how to create new Sources, Destinations and Connections'
   },
   {
     image: 'https://d1r1e7xjkfj7nz.cloudfront.net/connect5b.png',
-    title: 'Model Gen',
+    title: 'Model Gen Manager',
     organization: 'Document',
-    level: 'Advanced',
-    type: 'Course',
+    level: 'Intermediate',
+    type: 'Module',
     href: '/LMS-7',
-    category: 'Model Gen'
+    category: 'Model Gen',
+    description: 'Introduction to Vue’s Model Gen Manager, what it is capable of and how to use it'
+  },
+  {
+    image: 'https://d1r1e7xjkfj7nz.cloudfront.net/connect5b.png',
+    title: 'Workflow ',
+    organization: 'Document',
+    level: 'Intermediate',
+    type: 'Module',
+    href: '/LMS-5',
+    category: 'Document',
+    description: 'Introduction to Workflows, what the basics are and how to create them'
   }
 ];
 
