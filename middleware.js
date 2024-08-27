5088a3d38defae074eb68b74dd4933e7379ad2b6
silasashar1 <silas@madstreetden.com>
2024-08-27 14:57:52 +0530

    middleware

--- middleware.js
+++ middleware.js
@@ -1,19 +1,19 @@
-// middleware.js
+import { NextResponse } from 'next/server';
 
 export function middleware(req) {
     // Allowed referer domain
-    const allowedReferer = 'https://app.vue.ai/supportsite';
+    const allowedReferer = 'https://app.vue.ai';
     
     // Get the referer header
     const referer = req.headers.get('referer');
     
     // Check if the referer is present and starts with the allowed domain
     if (!referer || !referer.startsWith(allowedReferer)) {
-        return new Response('Access Forbidden', { status: 403 });
+        return new NextResponse('Access Forbidden', { status: 403 });
     }
 
     // Allow the request to proceed
-    return new Response(null, { status: 200 });
+    return NextResponse.next();
 }
 
 // Specify paths to apply the middleware
