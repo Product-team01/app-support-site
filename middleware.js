be935218125e0a40fd4c98d003713905087f3459
silasashar1 <silas@madstreetden.com>
2024-08-27 15:18:22 +0530

    lms

--- middleware.js
+++ middleware.js
@@ -1,20 +1,14 @@
 export default function middleware(req) {
-    // Allowed referer domain
-    const allowedReferer = 'https://app.vue.ai/supportsite';
-    
-    // Get the referer header
+    const allowedReferer = 'https://app.vue.ai/supportsite'; // Replace with your App B's domain
     const referer = req.headers.get('referer');
-    
-    // Check if the referer is present and starts with the allowed domain
+
     if (!referer || !referer.startsWith(allowedReferer)) {
         return new Response('Access Forbidden', { status: 403 });
     }
 
-    // If the referer is valid, let the request proceed
     return new Response(null, { status: 200 });
 }
 
-// Specify paths to apply the middleware
 export const config = {
     matcher: '/:path*',
 };
